cd02c84cf8093a729ac926161bb9fcdd72f9cbc2
Bhaskar Srivastava <bhasriva@publicisgroupe.net>
2018-06-29 15:58:12 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,11 @@
+/* Hello, World! program in node.js */
+/* console.log("Hello, World!") */
+
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
+
+console.log('Server started');
